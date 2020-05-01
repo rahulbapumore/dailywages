4a5d5bee2c7a9389ae4bfa89f3a7511e0d75d535
Rahul <rahulbapumore@gmail.com>
2020-05-01 21:57:43 +0530

    Computation

--- dailywages.sh
+++ dailywages.sh
@@ -4,3 +4,34 @@ hourlywage=20
 salary=0
 twhours=0
 day=0
+
+
+
+echo "Welcome to Wage Computation Program"
+echo "-----------------------------------"
+while [ $twhours -le 100 ]
+do
+	status=$(($RANDOM%2))
+	case $status in
+	1)	whours=$((RANDOM%10))
+		if [ $(($whours+$twhours)) -le 100 ]
+		then
+		dailywage=$(( $whours * $hourlywage ))
+		salary=$(( $salary + $dailywage ))
+		day=$(($day + 1))
+			if [ $whours -lt 9 ]
+			then
+				echo "(Part-Time) : Present : Day-$day : $dailywage = $whours x $hourlywage"
+			else
+				echo "(Full-Time) : Present : Day-$day : $dailywage = $whours x $hourlywage"
+			fi
+			twhours=$(($twhours + $whours))
+		else
+			break
+		fi
+	;;
+	0)	echo "Absent."
+		day=$(($day + 1))
+	;;
+	esac
+done
